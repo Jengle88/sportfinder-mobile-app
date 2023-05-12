d372a7d6402066e2bd6bf98b9c6d6f86542b0e32
Evgeny Shkvirya <zhshkvir@yandex.ru>
2023-05-12 19:32:03 +0300

    fix

--- app/src/main/java/ru/riders/sportfinder/screen/AuthorizationScreen.kt
+++ app/src/main/java/ru/riders/sportfinder/screen/AuthorizationScreen.kt
@@ -1,5 +1,6 @@
 package ru.riders.sportfinder.screen
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -22,6 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -29,6 +31,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import ru.riders.sportfinder.MainActivityViewModel
 import ru.riders.sportfinder.R
 import ru.riders.sportfinder.ui.theme.LightGreen
 import ru.riders.sportfinder.ui.theme.White
