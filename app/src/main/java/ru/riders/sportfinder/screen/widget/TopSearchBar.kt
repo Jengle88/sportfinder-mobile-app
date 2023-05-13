be7bb3d4c94fe9a083df6eeaa2da10d449126ece
Evgeny Shkvirya <zhshkvir@yandex.ru>
2023-05-13 19:28:28 +0300

    finish

--- app/src/main/java/ru/riders/sportfinder/screen/widget/TopSearchBar.kt
+++ app/src/main/java/ru/riders/sportfinder/screen/widget/TopSearchBar.kt
@@ -10,18 +10,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.TextFieldColors
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -32,7 +28,6 @@ import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
 import ru.riders.sportfinder.R
-import ru.riders.sportfinder.ui.theme.Green
 import ru.riders.sportfinder.ui.theme.LightGreen
 import ru.riders.sportfinder.ui.theme.SportFinderLightColorScheme
 import ru.riders.sportfinder.ui.theme.White
@@ -79,7 +74,7 @@ fun TopSearchBar(onTextSearchChanged: (String) -> Unit) {
                 .fillMaxWidth()
                 .layoutId("searchTextField")
                 .height(48.dp),
-            value = "TEST",//searchText
+            value = searchText,//
             textStyle = TextStyle(
                 color = White,
                 fontSize = 14.sp),
