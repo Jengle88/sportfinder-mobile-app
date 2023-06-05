845e029983e976ce92b31f6dcf26797ee0f4d8a3
Evgeny Shkvirya <zhshkvir@yandex.ru>
2023-06-05 10:41:39 +0300

    create screens subgraphs for navhost

--- app/src/main/java/ru/riders/sportfinder/screen/navigation/NavHost.kt
+++ app/src/main/java/ru/riders/sportfinder/screen/navigation/NavHost.kt
@@ -8,6 +8,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import androidx.navigation.navigation
 import ru.riders.sportfinder.screen.auth_screens.authorization_screen.AuthorizationScreen
 import ru.riders.sportfinder.screen.auth_screens.registration_screen.RegistrationScreen
 import ru.riders.sportfinder.screen.common_components.JCMapView
@@ -24,62 +25,84 @@ fun MainScreenNavHost(
     navHostController: NavHostController,
     isSupportedBottomNav: MutableState<Boolean>
 ) {
-    NavHost(navController = navHostController, startDestination = Screens.AUTH_SCREEN.route) {
-        composable(route = Screens.AUTH_SCREEN.route) {
-            isSupportedBottomNav.value = false
-            AuthorizationScreen(
-                navigateToProfileScreen = { navHostController.navigate(Screens.PROFILE_SCREEN.route) },
-                navigateToRegistrationScreen = { navHostController.navigate(Screens.REG_SCREEN.route) },
-            )
+    NavHost(navController = navHostController, startDestination = ScreensSubgraphs.AUTH.route) {
+        navigation(
+            startDestination = Screens.AUTH_SCREEN.route,
+            route = ScreensSubgraphs.AUTH.route
+        ) {
+            composable(route = Screens.AUTH_SCREEN.route) {
+                isSupportedBottomNav.value = false
+                AuthorizationScreen(
+                    navigateToProfileScreen = { navHostController.navigate(Screens.PROFILE_SCREEN.route) {
+                        popUpTo(ScreensSubgraphs.AUTH.route) {
+                            inclusive = true
+                        }
+                    } },
+                    navigateToRegistrationScreen = { navHostController.navigate(Screens.REG_SCREEN.route) },
+                )
+            }
+            composable(route = Screens.REG_SCREEN.route) {
+                isSupportedBottomNav.value = false
+                RegistrationScreen(
+                    navigateToProfileScreen = { navHostController.navigate(Screens.PROFILE_SCREEN.route) {
+                        popUpTo(ScreensSubgraphs.AUTH.route) {
+                            inclusive = true
+                        }
+                    } },
+                    navigateToAuthorizationScreen = { navHostController.navigate(Screens.AUTH_SCREEN.route) }
+                )
+            }
         }
-        composable(route = Screens.REG_SCREEN.route) {
-            isSupportedBottomNav.value = false
-            RegistrationScreen(
-                navigateToProfileScreen = { navHostController.navigate(Screens.PROFILE_SCREEN.route) },
-                navigateToAuthorizationScreen = { navHostController.navigate(Screens.AUTH_SCREEN.route) }
-            )
-        }
-        composable(route = Screens.PROFILE_SCREEN.route) {
-            isSupportedBottomNav.value = true
-            ProfileScreen()
-        }
-        composable(route = Screens.SPORT_COURT_MAP_SCREEN.route) {
-            isSupportedBottomNav.value = true
-            val jcMapView = JCMapView(LocalContext.current)
 
-            SportCourtMapScreen(
-                jcMapView,
-                navigateToSportCourtListScreen = { navHostController.navigate(Screens.SPORT_COURT_LIST_SCREEN.route) }
-            )
+        navigation(startDestination = Screens.PROFILE_SCREEN.route, route = ScreensSubgraphs.PROFILE.route) {
+            composable(route = Screens.PROFILE_SCREEN.route) {
+                isSupportedBottomNav.value = true
+                ProfileScreen()
+            }
         }
-        composable(route = Screens.SPORT_COURT_LIST_SCREEN.route) {
-            isSupportedBottomNav.value = true
-            SportCourtsListScreen(
-                navigateToSportCourtMapScreen = { navHostController.navigate(Screens.SPORT_COURT_MAP_SCREEN.route) }
-            )
-        }
-        composable(route = Screens.RUNNING_TRACKS_LIST_SCREEN.route) {
-            isSupportedBottomNav.value = true
-            RunningTracksListScreen(
-                navigateToWatchRunningTrackScreen = { trackId -> navHostController.navigate(Screens.WATCH_RUNNING_TRACK_SCREEN.route + "/$trackId") },
-                navigateToCreateTrackScreen = { navHostController.navigate(Screens.CREATE_TRACK_SCREEN.route) }
-            )
-        }
-        composable(route = Screens.WATCH_RUNNING_TRACK_SCREEN.route + "/{trackInfoNumber}",
-            arguments = listOf(navArgument("trackInfoNumber") {
-                type = NavType.IntType
-            })) {
-            val jcMapView = JCMapView(LocalContext.current)
 
-            isSupportedBottomNav.value = true
-            WatchRunningTrackScreen(jcMapView)
+        navigation(startDestination = Screens.SPORT_COURT_MAP_SCREEN.route, route = ScreensSubgraphs.SPORT_COURT.route) {
+            composable(route = Screens.SPORT_COURT_MAP_SCREEN.route) {
+                isSupportedBottomNav.value = true
+                val jcMapView = JCMapView(LocalContext.current)
+
+                SportCourtMapScreen(
+                    jcMapView,
+                    navigateToSportCourtListScreen = { navHostController.navigate(Screens.SPORT_COURT_LIST_SCREEN.route) }
+                )
+            }
+            composable(route = Screens.SPORT_COURT_LIST_SCREEN.route) {
+                isSupportedBottomNav.value = true
+                SportCourtsListScreen(
+                    navigateToSportCourtMapScreen = { navHostController.navigate(Screens.SPORT_COURT_MAP_SCREEN.route) }
+                )
+            }
         }
-        composable(route = Screens.CREATE_TRACK_SCREEN.route) {
-            isSupportedBottomNav.value = true
 
-            val jcMapView = JCMapView(LocalContext.current)
+        navigation(startDestination = Screens.RUNNING_TRACKS_LIST_SCREEN.route, route = ScreensSubgraphs.RUNNING_TRACK.route) {
+            composable(route = Screens.RUNNING_TRACKS_LIST_SCREEN.route) {
+                isSupportedBottomNav.value = true
+                RunningTracksListScreen(
+                    navigateToWatchRunningTrackScreen = { trackId -> navHostController.navigate(Screens.WATCH_RUNNING_TRACK_SCREEN.route + "/$trackId") },
+                    navigateToCreateTrackScreen = { navHostController.navigate(Screens.CREATE_TRACK_SCREEN.route) }
+                )
+            }
+            composable(route = Screens.WATCH_RUNNING_TRACK_SCREEN.route + "/{trackInfoNumber}",
+                arguments = listOf(navArgument("trackInfoNumber") {
+                    type = NavType.IntType
+                })) {
+                val jcMapView = JCMapView(LocalContext.current)
 
-            CreateRunningTrackScreen(jcMapView)
+                isSupportedBottomNav.value = true
+                WatchRunningTrackScreen(jcMapView)
+            }
+            composable(route = Screens.CREATE_TRACK_SCREEN.route) {
+                isSupportedBottomNav.value = true
+
+                val jcMapView = JCMapView(LocalContext.current)
+
+                CreateRunningTrackScreen(jcMapView)
+            }
         }
     }
 }
