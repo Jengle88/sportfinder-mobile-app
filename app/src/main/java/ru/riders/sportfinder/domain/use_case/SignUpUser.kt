8309fdf195bb6e225b3754481f43a7aa66aa00e7
Evgeny Shkvirya <zhshkvir@yandex.ru>
2023-06-20 21:12:51 +0300

    replace logic from usecase

--- app/src/main/java/ru/riders/sportfinder/domain/use_case/SignUpUser.kt
+++ app/src/main/java/ru/riders/sportfinder/domain/use_case/SignUpUser.kt
@@ -4,14 +4,11 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 import ru.riders.sportfinder.common.ApiResultState
-import ru.riders.sportfinder.data.db.UserProfileDao
-import ru.riders.sportfinder.data.remote.dto.toUserTokenEntity
 import ru.riders.sportfinder.domain.repository.UserProfileRepository
 import java.io.IOException
 import javax.inject.Inject
 
 class SignUpUser @Inject constructor(
-    private val userProfileDao: UserProfileDao,
     private val userProfileRepository: UserProfileRepository
 ) {
     operator fun invoke(login: String, password: String): Flow<ApiResultState<Boolean>> =
@@ -20,8 +17,7 @@ class SignUpUser @Inject constructor(
                 emit(ApiResultState.Loading())
 
                 val signUpData = userProfileRepository.signUpUser(login, password)
-                userProfileDao.insertUserProfile(signUpData.toUserTokenEntity())
-                emit(ApiResultState.Success(true))
+                emit(ApiResultState.Success(signUpData.token.isNotEmpty()))
             } catch (e: HttpException) {
                 emit(ApiResultState.Error(e.localizedMessage ?: "An unexpected error occurred"))
             } catch (e: IOException) {
